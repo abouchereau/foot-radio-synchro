7c95be7f52d6492fc0d438fc398142278d441572
anto <anthonybouchereau@hotmail.com>
2024-05-04 20:14:18 +0200

    add vue js

--- components/SeekButtons.vue
+++ components/SeekButtons.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
     <div class="btn-container">
-      <button @click="main.addDelay(10)">-10</button>
+      <button @click="main.addDelay(10)" :disabled="main.audioElement.currentTime<10">-10</button>
     </div>
     <div class="btn-container">
-      <button @click="main.addDelay(2)">-2</button>
+      <button @click="main.addDelay(2)" :disabled="main.audioElement.currentTime<2">-2</button>
     </div>
     <div class="btn-container">
-      <button @click="main.addDelay(0.5)">-0.5</button>
+      <button @click="main.addDelay(0.5)" :disabled="main.audioElement.currentTime<0.5">-0.5</button>
     </div>
     <div class="btn-container">
       <button v-if="main.isPaused" @click="main.resume()">PLAY</button>
       <button v-else @click="main.pause()">PAUSE</button>
     </div>
     <div class="btn-container">
-      <button @click="main.addDelay(-0.5)">+0.5</button>
+      <button @click="main.addDelay(-0.5)" :disabled="main.delay<0.5">+0.5</button>
     </div>
     <div class="btn-container">
-      <button @click="main.addDelay(-2)">+2</button>
+      <button @click="main.addDelay(-2)" :disabled="main.delay<2">+2</button>
     </div>
     <div class="btn-container">
-      <button @click="main.addDelay(-10)">+10</button>
+      <button @click="main.addDelay(-10)" :disabled="main.delay<10">+10</button>
     </div>
   </div>
 </template>
