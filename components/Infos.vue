7fe18c9d28bc0092ca0bdd54fae9432c77ff6f0c
abouchereau <anthonybouchereau@hotmail.com>
2024-05-06 17:48:19 +0200

    design, infos

--- components/Infos.vue
+++ components/Infos.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="pure-g mt-5">
-    <div class="pure-u-1 text-center">
+    <div class="pure-u-1-4"></div>
+    <div class="pure-u-2-4 text-center">
       <p>Vis tes matchs intensément en écoutant les commentaires de la radio !</p>
-      <p>Il y a généralement un décalage entre l'image à la TV et le son de la radio. Foot Radio Synchro te permet de caler ta raio préférée avec l'image la TV.</p>
+      <p>Il y a généralement un décalage entre l'image à la TV et le son de la radio. Foot Radio Synchro te permet de caler ta radio préférée avec l'image la TV.</p>
       <p>Attention : la synchronisation se fait <u>"manuellement"</u> : il faut ajuster soi-même le retard à appliquer au son de la radio.</p>
       <p>Comment faire ? Quand le commentateur radio évoque une action précise, faire PAUSE, et quand tu vois cette action à la TV, fais PLAY pour remettre le son en route.</p>
       <p>Décalages moyens constatés :</p>
-      <table>
+      <table class="pure-table">
         <thead>
         <tr>
           <th>Support</th>
@@ -19,6 +20,7 @@
           <tr><td>Site streaming</td><td>60 à 120 secondes</td></tr>
         </tbody>
       </table>
+      <p>Quelle radio suivre ? Voir <a target="_blank" href="https://www.radio.fr/sport">https://www.radio.fr/sport</a></p>
     </div>
   </div>
 </template>
